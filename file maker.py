bbbb47810efe3e3aeb1f50f73a1041305a4a19ca
vilius16 <34285916+vilius16@users.noreply.github.com>
2017-12-06 20:38:00 +0000

    Add files via upload

---
+++ file maker.py
@@ -0,0 +1,45 @@
+fName="easy_history_questions"
+accesmode="w"
+myF= open(fName, accesmode)
+
+
+fName="medium-history_questions"
+accesmode="w"
+myF= open(fName, accesmode)
+
+
+fName="hard_history_questions"
+accesmode="w"
+myF= open(fName, accesmode)
+
+
+fName="easy_assasins_creed_questions"
+accesmode="w"
+myF= open(fName, accesmode)
+
+
+fName="medium-assasins_creed_questions"
+accesmode="w"
+myF= open(fName, accesmode)
+
+
+fName="hard_assasins_creed_questions"
+accesmode="w"
+myF= open(fName, accesmode)
+
+
+fName="easy_COD_questions"
+accesmode="w"
+myF= open(fName, accesmode)
+
+
+fName="medium_COD_questions"
+accesmode="w"
+myF= open(fName, accesmode)
+
+
+fName="hard_COD_questions"
+accesmode="w"
+myF= open(fName, accesmode)
+
+
